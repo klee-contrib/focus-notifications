7b40fb2ab3d2ce43d213e52a862ff60f27ab60cc
Pierre Besson <pierre.besson7@gmail.com>
2015-10-12 13:52:16 +0200

    [notification] Notification commponent.

--- src/component/notification.js
+++ src/component/notification.js
@@ -1,29 +1,24 @@
 //Dependencies
 import React, { Component , PropTypes} from 'react';
 
-/**
-* Component use for uploading files.
-*/
-class Notification extends Component {
-    /** @inheritdoc */
-    constructor(props) {
-        super(props);
-    }
-    render(){
-        const {author, date, content, title, type} = this.props;
-        return (
-            <div data-focus='notification' data-type={type}>
-                <h2>{title}</h2>
-                <div>{content}</div>
-                <div>{date}</div>
-            </div>
-        );
-    }
-}
-
-Notification.displayName = 'Notification';
-Notification.propTypes = {
+const propTypes = {
+    title: PropTypes.string,
     content: PropTypes.string,
-    date: PropTypes.date
+    date: PropTypes.string,
+    type: PropTypes.string,
+    author: propTypes.string
 };
+
+const Notification = ({author, date, content, title, type}) => {
+    return (
+        <div dataFocus='notification' dataType={type}>
+            <h2>{title}</h2>
+            <div>{content}</div>
+            <div>{date}</div>
+        </div>
+    );
+};
+
+Notification.propTypes = propTypes;
+
 export default Notification;
