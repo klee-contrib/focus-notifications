d766415df959278a1b05ae4455f29e44a01f916d
Pierre Besson <pierre.besson7@gmail.com>
2015-10-12 16:46:53 +0200

    [dev and prod]

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,3 @@
 const devConfig =  require('webpack-focus').devConfig;
 var focusNotifConf = require('./focus-notifications-webpack.config');
-focusNotifConf.entry = [
-    './src/example/index'
-];
 module.exports = devConfig(focusNotifConf);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,9 @@
 console.log(require('webpack-focus'));
 const productionConfig =  require('webpack-focus').productionConfig;
 var focusNotifConf = require('./focus-notifications-webpack.config');
+focusNotifConf.entry = [
+    './src/component/index'
+];
 focusNotifConf.externals = {
     'focus-core': 'FocusCore',
     react: 'React',
