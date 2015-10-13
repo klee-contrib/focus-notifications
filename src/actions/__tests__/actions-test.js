9989b5db3e3d675422d23400b10835cb2a521f3e
Pierre Besson <pierre.besson7@gmail.com>
2015-10-13 14:47:59 +0200

    [test action type]

--- src/actions/__tests__/actions-test.js
+++ src/actions/__tests__/actions-test.js
@@ -1,19 +1,37 @@
-import {addNotification, readNotification, setVisibilityFilter, VisibilityFilters, ADD_NOTIFICATION, COMPLETE_NOTIFICATION, SET_VISIBILITY_FILTER} from '../'
+import {addNotification, readNotification, setVisibilityFilter, VisibilityFilters, ADD_NOTIFICATION, READ_NOTIFICATION, SET_VISIBILITY_FILTER} from '../'
 
 describe.only('actions', () => {
     it('actionType should be strings', () => {
         expect(ADD_NOTIFICATION).to.be.a('string');
-        expect(COMPLETE_NOTIFICATION).to.be.a('string');
+        expect(READ_NOTIFICATION).to.be.a('string');
         expect(SET_VISIBILITY_FILTER).to.be.a('string');
     } )
     describe('addNotification', () => {
         it('should be a function', () => {
             expect(addNotification).to.be.a('function');
         });
+        it('should return an object with a type and a notification property', () => {
+            const NOTIF = {content: 'lopez'};
+            const addNotificationResult = addNotification(NOTIF);
+            expect(addNotificationResult).to.be.an('object');
+            expect(addNotificationResult).to.have.ownProperty('type');
+            expect(addNotificationResult).to.have.ownProperty('notification');
+            expect(addNotificationResult.notification).to.deep.equal(NOTIF);
+            expect(addNotificationResult.type).to.equal(ADD_NOTIFICATION);
+        });
     });
     describe('readNotification', () => {
         it('should be a function', () => {
             expect(readNotification).to.be.a('function');
+        });
+        it('should return an object with a type and a notification property', () => {
+            const NOTIF_ID = 12345;
+            const readNotificationResult = readNotification(NOTIF_ID);
+            expect(readNotificationResult).to.be.an('object');
+            expect(readNotificationResult).to.have.ownProperty('type');
+            expect(readNotificationResult).to.have.ownProperty('notificationId');
+            expect(readNotificationResult.notificationId).to.deep.equal(NOTIF_ID);
+            expect(readNotificationResult.type).to.equal(READ_NOTIFICATION);
         });
     });
     describe('setVisibilityFilter', () => {
