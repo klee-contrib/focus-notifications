150ce02371bd293f985b418f6cc48254d9734b45
Pierre Besson <pierre.besson7@gmail.com>
2015-10-14 10:55:58 +0200

    [example] Update example to use provider.

--- src/example/index.js
+++ src/example/index.js
@@ -1,16 +1,21 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import NotificationCenter from '../component/notification-center';
+import notificationReducers from '../reducers';
 
+const store = createStore(notificationReducers);
 
-console.log('Exemple Notification center');
-
-const mockNotifs = [
-    {title: 'TITRE', content: 'CONTENT ...', date: new Date().toISOString(), type: 'info', author: 'joe.lopez@gmail.com'},
-    {title: 'TITRE 2', content: 'CONTENT 2 ...', date: new Date().toISOString(), type: 'info', author: 'david.lopez@gmail.com'},
-    {title: 'TITRE 3', content: 'CONTENT 3 ...', date: new Date().toISOString(), type: 'info', author: 'joe.lopez@gmail.com'}
-];
+const rootElement = document.getElementById('root');
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    ReactDOM.render(<NotificationCenter notificationList={mockNotifs}/>, document.getElementById('root'));
+    // The child must be wrapped in a function
+    // to work around an issue in React 0.13.
+    ReactDOM.render(<Provider store={store}>
+        <NotificationCenter />
+    </Provider>,
+    rootElement);
 });
