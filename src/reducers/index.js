099f44dfad6be20414b3728565abfe75637641a3
Pierre Besson <pierre.besson7@gmail.com>
2015-10-14 23:44:37 +0200

    [reducer] Add notification list to receive new notifs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import notificationList from './notifications-list';
 import visibilityFilter from './visibility-filter';
+import isFetching from './is-fetching';
 
+//This is a reduce function to ease reducer composition.
 export default combineReducers({
     visibilityFilter,
-    notificationList
+    notificationList,
+    isFetching
 });
