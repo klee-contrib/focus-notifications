484ea49627658faf300c494e217272c03fde9882
Pierre Besson <pierre.besson7@gmail.com>
2015-10-14 10:55:26 +0200

    [notification center] Connect the component to the reduce store.

--- src/component/notification-center.js
+++ src/component/notification-center.js
@@ -1,13 +1,42 @@
 //Dependencies
-import React, { Component , PropTypes} from 'react';
+import React, { Component , PropTypes } from 'react';
 import NotificationList from './notification-list';
+import NotificationAdd from './notification-add';
+import { connect } from 'react-redux';
+import { addNotification, readNotification, setVisibilityFilter } from '../actions';
 
-const NotificationCenter = ({notificationList}) => {
-    return (
-        <div data-focus='notification-center'>
-            <NotificationList data={notificationList} />
-        </div>
-    );
-};
+/**
+ * Notification.
+ */
+class NotificationCenter extends Component {
+    render() {
+        const {dispatch, notificationList} = this.props;
+        return (
+            <div data-focus='notification-center'>
+                <NotificationAdd onAddClick={text => dispatch(addNotification({content: text, author: 'joe.lopez@gmail.com', title: 'title', date: new Date().toISOString()}))} />
+                <NotificationList data={notificationList} />
+            </div>
+        );
+    }
+}
 
-export default NotificationCenter;
+NotificationCenter.displayName = 'NotificationCenter';
+
+NotificationCenter.propTypes = {
+    dispatch: PropTypes.func,
+    notificationList: PropTypes.array
+}
+
+// Select the notification from the state.
+function selectNotifications(notifications, filter) {
+    return notifications;
+}
+
+// Select the part of the state.
+function select(state) {
+    return {
+        notificationList: selectNotifications(state.notifications, state.visibilityFilter),
+        visibilityFilter: state.visibilityFilter
+    };
+}
+export default connect(select)(NotificationCenter);
