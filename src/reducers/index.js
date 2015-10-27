83f001251961ad3c50f9a1a2a72923f3a4e53da5
Pierre Besson <pierre.besson7@gmail.com>
2015-10-27 15:44:16 +0100

    [reducer] Add notificationReceived

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import notificationList from './notifications-list';
 import visibilityFilter from './visibility-filter';
 import isFetching from './is-fetching';
 import isOpen from './is-open';
+import notificationReceived from './notifications-received';
 //This is a reduce function to ease reducer composition.
 export default combineReducers({
     visibilityFilter,
     notificationList,
     isFetching,
-    isOpen
+    isOpen,
+    notificationReceived
 });
---
+++ src/reducers/notifications-received.js
@@ -0,0 +1,52 @@
+import { ADD_NOTIFICATION, ADD_NOTIFICATIONS, READ_NOTIFICATION, READ_NOTIFICATION_GROUP} from '../actions';
+import {RECEIVE_NOTIFICATIONS} from '../actions/fetch-notifications';
+import generateError from './util/error-generator';
+import {isObject, isArray, isString, isNumber} from 'lodash/lang';
+const REDUCER_NAME = 'NOTIFICATIONS_RECEIVED';
+
+function _addNotifToStateIfNeeded(state, notif) {
+    const {uuid} = notif;
+    if(!state[uuid]) {
+        state[uuid] = notif;
+    }
+    return state;
+}
+
+// reducers in charge of generatin the notification list
+export default function notificationsReceived(state = {}, action = {}) {
+    const {type, index, payload} = action;
+    switch (type) {
+        case ADD_NOTIFICATION:
+            if(!isObject(payload) || isArray(payload)) { throw new Error (generateError({name: REDUCER_NAME, action, expectedType: 'object'})); }
+            return _addNotifToStateIfNeeded(state, payload);
+        case ADD_NOTIFICATIONS:
+        case RECEIVE_NOTIFICATIONS:
+            if(!isArray(payload)) { throw new Error(generateError({name: REDUCER_NAME, action, expectedType: 'array'})); }
+            const data = action.payload.map((notif) => ({...notif, read: notif.read || false}));
+            return [...state, ...data];
+        case READ_NOTIFICATION:
+            if(!isString(payload) && !isNumber(payload)) { throw new Error(generateError({name: REDUCER_NAME, action, expectedType: 'string|number'})); }
+            const index = state.findIndex( (notif) => notif.uuid === action.payload);
+            if(index === -1) {
+                return state;
+            }
+            return [
+                ...state.slice(0, index),
+                //Add the read element to the index fitting the payload.
+                {...state[index], read: true},
+                ...state.slice(index + 1)
+            ];
+        case READ_NOTIFICATION_GROUP:
+            if(!isArray(payload)) { throw new Error(generateError({name: REDUCER_NAME, action, expectedType: 'array'})); }
+            const ids = payload;
+            //Reduce the state to change the read elements.
+            return state.reduce((newState, notif) => {
+                //The notif is already read or its index is in the read indexes.
+                const read = notif.read || ids.indexOf(notif.uuid) !== -1;
+                newState.push({...notif, read});
+                return newState;
+            }, []);
+        default:
+            return state;
+    }
+}
