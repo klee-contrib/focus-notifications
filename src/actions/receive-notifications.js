6b537c91aa463168e27c3e6c4e7c4ce9babcb4bd
Pierre Besson <pierre.besson7@gmail.com>
2015-10-28 09:09:01 +0100

    [actions] Add dismiss action.

--- src/actions/receive-notifications.js
+++ src/actions/receive-notifications.js
@@ -1,5 +1,8 @@
 export const RECEIVE_NEW_NOTIFICATIONS = 'RECEIVE_NEW_NOTIFICATIONS';
-
+export const DISMISS_NOTIFICATION = 'DISMISS_NOTIFICATION';
 export function receiveNotifications(newNotifications) {
     return { type: RECEIVE_NEW_NOTIFICATIONS, payload: newNotifications };
+}
+export function dismissNotification(notificationUuid) {
+    return { type: DISMISS_NOTIFICATION, payload: notificationUuid };
 }
