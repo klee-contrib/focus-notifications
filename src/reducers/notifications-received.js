c96d1a1ed723f57b046d8672c03a081b9c23e8a0
Pierre Besson <pierre.besson7@gmail.com>
2015-10-29 08:14:18 +0100

    [reducer] Add state into a new object because, redux concider it unchanged otherwise.

--- src/reducers/notifications-received.js
+++ src/reducers/notifications-received.js
@@ -10,13 +10,13 @@ function _addNotifToStateIfNeeded(state, notif) {
     if(!state[uuid]) {
         state[uuid] = notif;
     }
-    return state;
+    return {...state};
 }
 function _addReadToNotificationIfExists(state, uuid) {
     if(isObject(state[uuid])) {
         state[uuid] = {...state[uuid], displayed: true};
     }
-    return state;
+    return {...state};
 }
 
 // reducers in charge of generatin the notification list
@@ -30,7 +30,7 @@ export default function notificationsReceived(state = {}, action = {}) {
         case RECEIVE_NOTIFICATIONS:
             if(!isArray(payload)) { throw new Error(generateError({name: REDUCER_NAME, action, expectedType: 'array'})); }
             action.payload.forEach((notif) => _addNotifToStateIfNeeded(state, notif));
-            return state;
+            return {...state};
         case DISMISS_NOTIFICATION:
         case READ_NOTIFICATION:
             if(!isString(payload) && !isNumber(payload)) { throw new Error(generateError({name: REDUCER_NAME, action, expectedType: 'string|number'})); }
@@ -38,7 +38,7 @@ export default function notificationsReceived(state = {}, action = {}) {
         case READ_NOTIFICATION_GROUP:
             if(!isArray(payload)) { throw new Error(generateError({name: REDUCER_NAME, action, expectedType: 'array'})); }
             payload.forEach((notifUuid) => _addReadToNotificationIfExists(state, notifUuid));
-            return state;
+            return {...state};
         default:
             return state;
     }
