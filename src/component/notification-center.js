1aa030e46853326a4bf4ec527c6185e49c3979c7
Pierre Besson <pierre.besson7@gmail.com>
2015-11-01 23:26:13 +0100

    [notification panel] Create a standalone component.

--- src/component/notification-center.js
+++ src/component/notification-center.js
@@ -1,13 +1,13 @@
 //Dependencies
 import React, { Component , PropTypes } from 'react';
-import NotificationGroup from './notification-group';
-import NotificationAdd from './notification-add';
 import NotificationCenterIcon from './notification-center-icon';
 import NotificationReceiver from './notification-receiver';
+import NotificationPanel from './notification-panel';
 import { connect } from 'react-redux';
 import { addNotification, readNotification, readNotificationGroup, setVisibilityFilter, openCenter, closeCenter } from '../actions';
 import { fetchNotifications } from '../actions/fetch-notifications';
 import{ dismissNotification } from '../actions/receive-notifications';
+
 import polling from '../util/polling';
 // Notification center component
 class NotificationCenter extends Component {
@@ -33,23 +33,29 @@ class NotificationCenter extends Component {
 
         return (
             <div data-focus='notification-center'>
-                <NotificationCenterIcon number={unreadNotifs.length} openCenter={ () => dispatch(openCenter())}/>
-                {!isOpen && hasFetchedOnce && <NotificationReceiver data={notificationsReceived} onDismiss={ notifId => dispatch(dismissNotification(notifId)) }/>}
+                <NotificationCenterIcon
+                    number={unreadNotifs.length}
+                    openCenter={() => dispatch(openCenter())}
+                />
+                {
+                    !isOpen && hasFetchedOnce &&
+                    <NotificationReceiver
+                        data={notificationsReceived}
+                        onDismiss={notifId => dispatch(dismissNotification(notifId))}
+                    />
+                }
                 {
                     isOpen &&
-                    <div  data-fetching={isFetching} data-focus='notification-center-panel'>
-                        <header>
-                            <button data-focus='notification-center-close' className='mdl-button mdl-button--icon' onClick={() => dispatch(closeCenter())}>
-                              <i className="material-icons">clear</i>
-                            </button>
-                            <h1 onClick={() => dispatch(fetchNotifications())}>{`Notification center (${unreadNotifs.length})`}</h1>
-                        </header>
-                        {
-                            hasAddNotif &&
-                            <NotificationAdd onAddClick={data => dispatch(addNotification(data))} />
-                        }
-                        <NotificationGroup data={unreadNotifs} onGroupRead={data => dispatch(readNotificationGroup(data))} onSingleRead={data => dispatch(readNotification(data))} />
-                    </div>
+                        <NotificationPanel
+                            hasAddNotif={hasAddNotif}
+                            isFetching={isFetching}
+                            onAddClick={data => dispatch(addNotification(data))}
+                            onClosePanel={() => dispatch(closeCenter())}
+                            onGroupRead={data => dispatch(readNotificationGroup(data))}
+                            onSingleRead={data => dispatch(readNotification(data))}
+                            onTitleClick={() => dispatch(fetchNotifications())}
+                            unreadNotifs={unreadNotifs}
+                        />
                 }
             </div>
         );
