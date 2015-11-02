009c30140db53e93470e7c6bef5c61751a117cbb
Pierre Besson <pierre.besson7@gmail.com>
2015-11-02 23:54:58 +0100

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,7 @@ console.log(require('webpack-focus'));
 const productionConfig =  require('webpack-focus').productionConfig;
 var focusNotifConf = require('./focus-notifications-webpack.config');
 focusNotifConf.entry = [
-    './src/component/index'
+    './src/index'
 ];
 focusNotifConf.externals = {
     'focus-core': 'FocusCore',
