522c5117e284c38c9b62ef2050860581f61e2905
Pierre Besson <pierre.besson7@gmail.com>
2016-02-01 15:00:36 +0100

    [group] Secure sort.

--- src/component/notification-group.js
+++ src/component/notification-group.js
@@ -2,7 +2,7 @@
 import React, { Component , PropTypes} from 'react';
 import NotificationList from './notification-list';
 import {capitalize} from 'lodash/string';
-import {groupBy, map, pluck} from 'lodash/collection';
+import {groupBy, reduce, pluck} from 'lodash/collection';
 import {getConfig} from '../config';
 import moment from 'moment';
 const propTypes = {
@@ -16,15 +16,15 @@ function _isYoungerThanA(periodName, date) {
 // function to group date
 function groupDate({creationDate: date}) {
     if(_isYoungerThanA('day', date)) {
-        return 'today';
+        return '0_today';
     }
     if(_isYoungerThanA('week', date)) {
-        return 'week';
+        return '1_lastWeek';
     }
     if(_isYoungerThanA('month', date)) {
-        return 'month';
+        return '2_lastMonth';
     }
-    return 'before';
+    return '3_before';
 }
 
 function translate(key){
@@ -32,34 +32,37 @@ function translate(key){
   return i18n[key];
 }
 
-function formatDate(date) {
-    if(_isYoungerThanA('day', date)) {
-        return momentDate.fromNow();
-    }
-    if(_isYoungerThanA('week', date)) {
-        return 'Last week';
-    }
-    if(_isYoungerThanA('month', date)) {
-        return 'Last month';
-    }
-    return 'Before';
-}
-
 function sortDateFn(a, b){
   return new Date(b.creationDate) - new Date(a.creationDate);
 }
 
+function sortNameFn(a,b){
+  if (a.key < b.key)
+    return -1;
+  else if (a.key > b.key)
+    return 1;
+  else
+    return 0;
+}
+
 //Maybe i should add a Notification Group component by date which uses a notifciation list component
 const NotificationGroup = ({data, onGroupRead, onSingleRead}) => {
-    let idx = 0; // maybe use sth else as key
+    //    console.log('Grouped infos', groupBy(data, groupDate), reduce(groupBy(data, groupDate));
+    const groupedAndSortedNotifs = reduce(
+      groupBy(data, groupDate),
+      (accumulator, value, key) => {
+          accumulator.push({key, value});
+          return accumulator;
+      },
+      []
+    ).sort(sortNameFn);
     return (
         <div data-focus='notification-group'>
             {
-                map(
-                    groupBy(data, groupDate),
-                    (group, groupTitle) => {
+                groupedAndSortedNotifs.map(
+                    ({value: group, key: groupTitle}) => {
                         return (
-                            <div key={idx++}>
+                            <div key={groupTitle}>
                                 <h2>{translate(groupTitle)} <button className='mdl-button mdl-js-button mdl-button--icon' onClick={()=> onGroupRead(pluck(group, 'uuid'))}><i className='material-icons'>done_all</i></button></h2>
                                 <NotificationList data={group.sort(sortDateFn)} onRead={onSingleRead}/>
                             </div>
