7c37cc6d76b4ac24662511d45622c8892f3cfb53
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-11 16:21:12 +0200

    clean index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React , { Component , PropTypes }from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import createStore from './store/create';
 import NotificationCenter from './component/notification-center';
-import {extendConfig, getConfig} from './config';
+import { extendConfig, getConfig } from './config';
 import DevTools from './container/dev-tools';
 
 //Import sass files
@@ -11,52 +11,124 @@ import './style';
 const isDev = __DEV__;
 const notificationStore = createStore();
 
-function NotificationCenterDev({iconName, onSingleClick, store, panelFooter, panelHeader}) {
+/**
+ * Render notification center in dev mode.
+ * @param {object} props props.
+ * @returns {JsxElement} element.
+ */
+function NotificationCenterDev({ iconName, onSingleClick, store, panelFooter, panelHeader }) {
     return (
         <Provider store={store}>
             <div>
-                <NotificationCenter iconName={iconName} hasAddNotif={false} onSingleClick={onSingleClick} panelHeader={panelHeader} panelFooter={panelFooter}/>
-                <DevTools/>
+                <NotificationCenter
+                    iconName={iconName}
+                    hasAddNotif={false}
+                    onSingleClick={onSingleClick}
+                    panelHeader={panelHeader}
+                    panelFooter={panelFooter}
+                />
+                <DevTools />
             </div>
         </Provider>
     );
 }
 NotificationCenterDev.displayName = 'NotificationCenterDev';
+NotificationCenterDev.propTypes = {
+    iconName: PropTypes.string.isRequired,
+    onSingleClick: PropTypes.func.isRequired,
+    store: PropTypes.object.isRequired,
+    panelFooter: PropTypes.element,
+    panelHeader: PropTypes.element
+};
+NotificationCenterDev.defaultProps = {
+    panelFooter: undefined,
+    panelHeader: undefined
+};
 
-
-
-function NotificationCenterProd({iconName, onSingleClick, store, panelFooter, panelHeader}) {
+/**
+ * Render notification center in production mode.
+ * @param {object} props props.
+ * @returns {JsxElement} element.
+ */
+function NotificationCenterProd({ iconName, onSingleClick, store, panelFooter, panelHeader }) {
     return (
         <Provider store={store}>
-            <NotificationCenter iconName={iconName} hasAddNotif={false} onSingleClick={onSingleClick} panelHeader={panelHeader} panelFooter={panelFooter} />
+            <NotificationCenter
+                iconName={iconName}
+                hasAddNotif={false}
+                onSingleClick={onSingleClick}
+                panelHeader={panelHeader}
+                panelFooter={panelFooter}
+            />
         </Provider>
     );
 }
 NotificationCenterProd.displayName = 'NotificationCenterProd';
+NotificationCenterProd.propTypes = {
+    iconName: PropTypes.string.isRequired,
+    onSingleClick: PropTypes.func.isRequired,
+    store: PropTypes.object.isRequired,
+    panelFooter: PropTypes.element,
+    panelHeader: PropTypes.element
+};
+NotificationCenterProd.defaultProps = {
+    panelFooter: undefined,
+    panelHeader: undefined
+};
 
 
-
+/**
+ * Render notification center.
+ * @param {object} props props.
+ * @returns {JsxElement} element.
+ */
 class SmartNotificationCenter extends Component {
+
+    static displayName = 'SmartNotificationCenter';
+
+    static propTypes = {
+        config: PropTypes.object.isRequired,
+        iconName: PropTypes.string.isRequired,
+        onSingleClick: PropTypes.func.isRequired,
+        store: PropTypes.object.isRequired,
+        panelFooter: PropTypes.element,
+        panelHeader: PropTypes.element
+    };
+
+    static defaultProps = {
+        panelFooter: undefined,
+        panelHeader: undefined
+    };
+
+    /** @inheritdoc */
     componentWillMount() {
-        const {config} = this.props;
+        const { config } = this.props;
         extendConfig(config);
-        const {translateText, translateDate} = getConfig();
-        if(!translateText) {
+
+        const { translateText, translateDate } = getConfig();
+        if (!translateText) {
             console.error('please define a text translation function. ex: translateText: (text) => i18next.t(text)');
         }
-        if(!translateDate) {
+        if (!translateDate) {
             console.error('please define a date formatter function. ex: translateDate: (date) => moment(date).forNow()');
         }
     }
+
+    /** @inheritdoc */
     render() {
-        const {iconName, panelFooter, panelHeader} = this.props;
+        const { iconName, onSingleClick, panelFooter, panelHeader } = this.props;
         const NotificationCenterComponent = isDev ? NotificationCenterDev : NotificationCenterProd;
-        return <NotificationCenterComponent iconName={iconName} onSingleClick={this.props.onSingleClick} store={notificationStore} panelHeader={panelHeader} panelFooter={panelFooter}/>
+
+        return (
+            <NotificationCenterComponent
+                iconName={iconName}
+                onSingleClick={onSingleClick}
+                store={notificationStore}
+                panelHeader={panelHeader}
+                panelFooter={panelFooter}
+            />
+        );
     }
 }
-SmartNotificationCenter.displayName = 'SmartNotificationCenter';
-SmartNotificationCenter.propTypes = {
-    config: PropTypes.object,
-    iconName: PropTypes.string
-};
+
 export default SmartNotificationCenter;
